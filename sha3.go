8e2d86b024a8d6cd43ee2aa59aba536d2bf8089f
ouwenkg <2630582710@qq.com>
2019-07-06 17:35:22 +0800

    [refactor]remove binary and split logics

--- sha3.go
+++ sha3.go
@@ -1,10 +1,8 @@
-package main
+package hasher
 
 import (
 	"fmt"
     "github.com/ebfe/keccak"
-    "golang.org/x/crypto/blake2b"
-    "hasher/sm3"
 )
 
 type Hasher interface {
@@ -14,12 +12,6 @@ type Hasher interface {
 type Sha3 struct {
 }
 
-type Blake2b struct {
-}
-
-type Sm3 struct {
-}
-
 func (sha3 Sha3) digest(data []byte) []byte {
     hasher := keccak.New256()
     sum := hasher.Sum(data)
@@ -27,29 +19,15 @@ func (sha3 Sha3) digest(data []byte) []byte {
 	return sum
 }
 
-func (b Blake2b) digest(data []byte) []byte {
-    hasher, _ := blake2b.New256([]byte("AsceticBear"))
-    sum := hasher.Sum(data)
-	fmt.Println("digest blake2b...", sum)
-	return sum
-}
+// func main() {
+// 	var hasher Hasher
 
-func (s Sm3) digest(data []byte) []byte {
-    hasher := sm3.New()
-    sum := hasher.Sum(data)
-    fmt.Println("digest sm3...", sum)
-    return sum
-}
+// 	hasher = new(Sha3)
+// 	hasher.digest([]byte(""))
 
-func main() {
-	var hasher Hasher
-
-	hasher = new(Sha3)
-	hasher.digest([]byte(""))
-
-	hasher = new(Blake2b)
-    hasher.digest([]byte(""))
+// 	hasher = new(Blake2b)
+//     hasher.digest([]byte(""))
     
-    hasher = new(Sm3)
-    hasher.digest([]byte(""))
-}
+//     hasher = new(Sm3)
+//     hasher.digest([]byte(""))
+// }
